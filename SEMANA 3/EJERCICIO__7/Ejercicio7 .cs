787bfb9fbe14677329d9aa667cde425946e52e74
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 23:36:00 -0500

    Add files via upload

---
+++ SEMANA 3/EJERCICIO__7/Ejercicio7 .cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ejercicio7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int rangoInicio, rangoFinal;
+
+            Console.WriteLine("Ingrese el rango inicial");
+            rangoInicio = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el rango final");
+            rangoFinal = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n\nLos numeros pares entre el rango " + rangoInicio + " - " + rangoFinal + " Son :\n");
+            for (int i = rangoInicio; i <= rangoFinal; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine("\n\nLos numeros impares entre el rango " + rangoInicio + " - " + rangoFinal + " Son :\n");
+            for (int i = rangoInicio; i <= rangoFinal; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
