eebe67dfbb1d4eb2304bbf6f90e05e7a3a70f095
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 23:33:03 -0500

    Add files via upload

---
+++ SEMANA 3/EJERCICIO__2/Ejercicio2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Por favor digite un numero:");
+		int a = int.Parse(Console.ReadLine());
+		Console.WriteLine("---------------");
+		for (int y = 1; y <= a; y++)
+		{
+			for (int x = 1; x <= a - y; x++)
+			{
+				Console.Write(" ");
+			}
+
+			for (int x = 1; x <= y; x++)
+			{
+				Console.Write("*");
+			}
+
+			for (int x = 1; x <= y - 1; x++)
+			{
+				Console.Write("*");
+			}
+
+			Console.WriteLine();
+		}
+		Console.WriteLine("---------------");
+	}
+}
