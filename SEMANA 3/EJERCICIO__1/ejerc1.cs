758d0c38def76040a80c3ed1e02c56803beb49e4
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 23:50:25 -0500

    Add files via upload

---
+++ SEMANA 3/EJERCICIO__1/ejerc1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int filas, num = 1;
+        Console.WriteLine("Introduce el número de líneas del triángulo: ");
+        filas = int.Parse(Console.ReadLine());
+        Console.WriteLine("---------------------------------------------------------");
+        for (int i = 1; i <= filas; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("\t" + num);
+                num++;
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("---------------------------------------------------------");
+    }
+
+}
