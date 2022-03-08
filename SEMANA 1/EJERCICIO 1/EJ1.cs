6e26c6c4251d813e83bda1a3923f49cc73d2504e
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 20:55:06 -0500

    Add files via upload

---
+++ SEMANA 1/EJERCICIO 1/EJ1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+
+
+        static void Main()
+        {
+            int numero;
+            Console.Write("Por favor ingresar un numero");
+            numero = int.Parse(Console.ReadLine());
+
+            {
+                if (numero > 0) ;
+
+                else
+                    Console.Write("El numero es:" + numero + " y su valor absoluto es:" + Math.Abs(numero));
+
+            }
+        }
+
+    }
+}
