8ed5b24fcd81c8b6544d196b3f436399f4d69d1e
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 21:51:57 -0500

    Add files via upload

---
+++ SEMANA 2/EJERCICIO3/Ejercicio3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            int num;
+            char fin;
+            do
+            {
+                Console.WriteLine("Ingrese un Numero mayor a 100");
+                a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el Segundo numero ");
+                b = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("1 = Suma 2 = Resta 3 = Multiplicacion 4 = Division");
+                num = int.Parse(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine(a + b);
+                        break;
+                    case 2:
+                        Console.WriteLine(a - b);
+                        break;
+                    case 3:
+                        Console.WriteLine(a * b);
+                        break;
+                    case 4:
+                        Console.WriteLine(a / b);
+                        break;
+                }
+                Console.WriteLine("CONTINUAR [S/N]");
+                fin = char.Parse(Console.ReadLine().ToUpper());
+
+            }
+        }
+    }
+}
