b2948ea1058b4211856647dad85f573f776aab20
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 23:41:03 -0500

    Add files via upload

---
+++ SEMANA 3/EJERCICIO__9/9.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ejercicio9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3;
+
+            Console.WriteLine("Primer Nivel: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Segundo Nivel: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Tercer Nivel: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            if (n1 == n2 && n1 == n3)
+                Console.WriteLine("Es Equilatero");
+
+            else
+            {
+                if (n1 == n2 || n1 == n3 || n3 == n2)
+                    Console.WriteLine("Es Isoceles");
+                else
+                {
+                    if (n1 != n2 || n1 != n3 || n3 != n2)
+                        Console.WriteLine("Es Escaleno");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
