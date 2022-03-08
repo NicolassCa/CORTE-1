5f5970f6fba979ce50c05da38f90d451172846df
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 23:34:37 -0500

    Add files via upload

---
+++ SEMANA 3/EJERCICIO__6/Ejercicio6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AplicationBase
+{
+    class program
+    {
+        static void Main()
+        {
+            int num;
+
+            Console.WriteLine("Digite numero par entre 100 y 200: ");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = num; i <= 200; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
