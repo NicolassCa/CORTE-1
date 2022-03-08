60aa3cfd0f80ef29edae001a31910b43a5c68c32
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 21:08:49 -0500

    Add files via upload

---
+++ SEMANA 1/EJERCICIO 9/EJ9.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AplicationBase
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+
+        {
+            decimal Resultado;
+            int EdadPersona;
+
+            Console.WriteLine("digite la edad");
+            EdadPersona = int.Parse(System.Console.ReadLine());
+
+            if (EdadPersona > 2)
+            {
+                Resultado = 21 + (EdadPersona - 2) * 4;
+            }
+            else
+            {
+                Resultado = (EdadPersona * 10);
+            }
+
+
+
+
+            Console.WriteLine("La edad del perro equivale en años humanos a: " + Resultado);
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
