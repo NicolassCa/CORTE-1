10ad954ddbca8ee72c5f8be7ac3d2b4fcab5fb7c
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 23:43:02 -0500

    Add files via upload

---
+++ SEMANA 3/EJERCICIO__DOCE/Ejercicio12.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+
+
+namespace AlfacaCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<Elector, bool> Electores = new Dictionary<Elector, bool>();
+            Dictionary<Aspirante, int> Aspirantes = new Dictionary<Aspirante, int>();
+
+            public void ExampleButtonClick() =>
+                Votar("Juan Osorio",
+                    "Sebastian Rodriguez");
+
+
+            public void Votar(string electorName, string aspiranteName)
+            {
+                bool _electorEncontrado = false;
+
+                foreach (var elector in Electores)
+                {
+                    if (elector.Key.Name == electorName)
+                    {
+                        _electorEncontrado = true;
+
+                        if (!elector.Value)
+                        {
+                            var aspirante = Aspirantes.Where(a => a.Key.Name ==
+                            aspiranteName).FirstOrDefault().Key;
+
+                            if (aspirante != null)
+                            {
+                                Aspirantes[aspirante]++;
+                                Electores[elector.Key] = true;
+                                break;
+                            }
+                            else throw new Exception("Este aspirante no ha sido encontrado en la base de datos");
+                        }
+                        else throw new Exception("Este elector ya ha votado!");
+                    }
+
+                }
+
+                if (!_electorEncontrado) throw new Exception("El elector no ha sido encontrado en la base de datos");
+            }
+        }
+
+        public class Elector : User
+        {
+            public Elector(string name) { Name = name; }
+        }
+
+        public class Aspirante : User
+        {
+            public Aspirante(string name) { Name = name; }
+        }
+
+        public class User
+        {
+            public string Name { get; set; }
+            public string Id { get { return ID.GetNew(); } }
+        }
+        public static class ID
+        {
+            private static int _count = 0;
+
+            public static string GetNew()
+            => (_count++).ToString("x4");
+        }
+
+
+
+    }
+}
+       
