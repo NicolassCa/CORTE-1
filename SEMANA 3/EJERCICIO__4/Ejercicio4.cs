96abd5512955e64aa4168fffab598db9d7a713d3
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 23:34:05 -0500

    Add files via upload

---
+++ SEMANA 3/EJERCICIO__4/Ejercicio4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AplicationBase
+{
+    class program
+    {
+        static void Main()
+        {
+            int num;
+            double resultado = 0;
+            Console.WriteLine("Digite numero entero para realizar la operacion: ");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                resultado = 1 + (1.0 / i);
+
+            }
+
+            Console.WriteLine("El resultado de la suma de fraccionarios es: " + resultado);
+        }
+    }
+}
