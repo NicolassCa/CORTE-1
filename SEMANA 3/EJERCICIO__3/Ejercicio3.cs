55a206c8fc58c8e29820738dafa7cf8147c15a77
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 23:33:31 -0500

    Add files via upload

---
+++ SEMANA 3/EJERCICIO__3/Ejercicio3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		int n;
+		int x;
+		Console.WriteLine(" Por favor ingrese N1:");
+		n = int.Parse(Console.ReadLine());
+		for (x = 1; x <= n; x++)
+		{
+			Console.WriteLine("*");
+		}
+
+		Console.WriteLine(" Por favor ingrese N2:");
+		n = int.Parse(Console.ReadLine());
+		for (x = 1; x <= n; x++)
+		{
+			Console.WriteLine("*");
+		}
+		Console.WriteLine(" Por favor ingrese N3:");
+		n = int.Parse(Console.ReadLine());
+		for (x = 1; x <= n; x++)
+		{
+			Console.WriteLine("*");
+		}
+		Console.WriteLine(" Por favor ingrese N4:");
+		n = int.Parse(Console.ReadLine());
+		for (x = 1; x <= n; x++)
+		{
+			Console.WriteLine("*");
+		}
+		Console.WriteLine(" Por favor ingrese N5:");
+		n = int.Parse(Console.ReadLine());
+
+
+		for (x = 1; x <= n; x++)
+		{
+			Console.WriteLine("*");
+		}
+
+	}
+
+}
