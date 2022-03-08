d176e041454913cc01f8b769f1db368dfb0dc5fb
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 23:40:41 -0500

    Add files via upload

---
+++ SEMANA 3/EJERCICIO__8/Ejercicio8 .cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace ejercicio
+{
+
+    class ejercicio
+    {
+        static void Main(String[] args)
+        {
+
+
+            acciones o = new acciones();
+            o.pedir_datos();
+            o.sumar();      
+            o.restar();
+            o.multiplicar();
+            o.dividir();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+
+class acciones
+{
+    double a, b;
+
+    public void pedir_datos()
+    {
+        Console.WriteLine("Digite el primer numero");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite el segundo numero");
+        b = int.Parse(Console.ReadLine());
+
+
+        while (a <= 0 || b <= 0)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite el primer numero");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite el segundo numero");
+            b = int.Parse(Console.ReadLine());
+
+        }
+
+    }
+
+
+    public void sumar()
+    {
+        Console.WriteLine("La suma de {0} + {1} es {2}", a, b, a + b);
+    }
+
+    public void restar()
+    {
+        Console.WriteLine("La resta de {0} - {1} es {2}", a, b, a - b);
+    }
+
+    public void multiplicar()
+    {
+        Console.WriteLine("La multiplicacion de{0} x {1} es {2}", a, b, a * b);
+    }
+
+    public void dividir()
+    {
+        Console.WriteLine("La divicion de {0} / {1} es {2}", a, b, a / b);
+    }
+
+}
