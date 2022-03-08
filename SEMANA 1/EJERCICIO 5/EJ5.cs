5efa375e5b2ebeeb26c5928b0d9c5fe2a8144909
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-03-07 21:01:37 -0500

    Add files via upload

---
+++ SEMANA 1/EJERCICIO 5/EJ5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RaizCuadradaCubica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double Numero = 0;
+            Console.WriteLine("digite el numero del Radicando");
+            Numero = double.Parse(System.Console.ReadLine());
+            double raizCuadrada = Math.Sqrt(Numero);
+            Console.WriteLine("La raíz cuadrada de {0} es {1}", Numero, raizCuadrada);
+        }
+    }
+}
